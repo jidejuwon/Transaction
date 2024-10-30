9f823645058473d1bc338fda860babef1d870a57
Ola <juwonolajide@gmail.com>
2024-10-30 11:14:04 +0100

    update user model and factory to exclude laravel fields user seeder for test

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,13 +4,14 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model;
+// use Illuminate\Foundation\Auth\User as Authenticatable;
+// use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Model
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory;
 
     /**
      * The attributes that are mass assignable.
@@ -21,29 +22,6 @@ class User extends Authenticatable
         'name',
         'email',
     ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
 
     public function transactions()
     {
--- database/migrations/2024_10_30_092518_create_transactions_table.php
+++ database/migrations/2024_10_30_092518_create_transactions_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->enum('type', ['debit', 'credit']);
             $table->decimal('previous_balance', 15, 2);
             $table->decimal('current_balance', 15, 2);
+            $table->string('reference');
             $table->timestamps();
         });
     }
